319ee9ca8a436e2d7f97665a375c2b320f0c21fe
Chase Swindle <acswindle@gmail.com>
2025-02-16 08:57:53 -0600

    Refactor into database setup

--- main.go
+++ main.go
@@ -9,38 +9,24 @@ import (
 	"log"
 )
 
-func run() error {
-	fmt.Println("Running Task Manager...")
+func setup() (context.Context, *database.Queries, error) {
 	ctx := context.Background()
 
 	db, err := sql.Open("sqlite3", "./db/task-manager.sqlite3")
 	if err != nil {
-		return err
+		return nil, &database.Queries{}, err
 	}
 
 	queries := database.New(db)
 
-	// list all authors
-	user := "John Doe"
-	id, err := queries.InsertUser(ctx, user)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Created user %s with ID: %d\n", user, id)
+	return ctx, queries, nil
 
-	users, err := queries.GetUsers(ctx)
-	if err != nil {
-		return err
-	}
-	for _, user := range users {
-		fmt.Printf("Found user %s with ID: %d\n", user.Name, user.ID)
-	}
-
-	return nil
 }
 
 func main() {
-	if err := run(); err != nil {
+	fmt.Println("Running Task Manager...")
+	_, _, err := setup()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
